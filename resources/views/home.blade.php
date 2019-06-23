74ff27461990410430a951b7b00996db7db7da75
Acro Smasher <161710148@smkassalaambandung.sch.id>
2019-06-23 21:15:51 +0700

    Revisi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
 <br>
 
 <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+    <div class="col-lg-12">
     <div class="panel panel-container">
             <div class="row">
                 <div class="col-xs-6 col-md-3 col-lg-6 no-padding">
@@ -36,6 +37,7 @@
                 </div>
             </div>
         </div>
+    </div>
     </div>
 
 
